5783bb963dedbcb4fa78b83a96ac025b8318c864
Ryan Hamilton <ryangilberthamilton@gmail.com>
2024-07-11 11:53:05 -0400

    refactor inital program

---
+++ gee2gpd/convert.py
@@ -0,0 +1,48 @@
+import ee
+import geopandas as gpd
+
+
+def img2feat(element: ee.Image) -> ee.feature.Feature:
+    """converts image to feature"""
+    data = ee.Image(element)
+    # copy all properties from the src image to the dest feature
+    geom = data.geometry()
+    prop_names = data.propertyNames()
+    props = data.toDictionary(prop_names)
+    return ee.Feature(geom, props)
+
+
+def ic_to_dataframe(collection: ee.ImageCollection):
+    """ handels the conversion from image collection to a geopandas dataframe 
+    assumes that some level of pre processing has been done i.e. filering by bounds and dates
+    """
+    features = collection.toList(collection.size()).map(img2feat)
+    fc = ee.FeatureCollection(features)
+    
+    # convert json output from fc to dataframe
+    geojson = fc.getInfo()
+    
+    # call class constructor for GeoDataFrame.features
+    gdf = gpd.GeoDataFrame.from_features(geojson['features'])
+    
+    # handles if there is no projection info
+    if gdf.crs is None:
+        gdf.set_crs(4326, inplace=True)
+    
+    # handles if the projection info is not wgs84
+    if gdf.crs != 4326:
+        gdf.to_crs(4326, inplace=True)
+    
+    # re arange the column names
+    column_names = gdf.columns.tolist()
+    if column_names[-1] != "geometry":
+        print("Geometry is not last")
+        geom = column_names.pop(column_names.index("geometry"))
+        print(geom)
+        column_names.insert(len(column_names), geom)
+
+    return gdf[column_names]
+
+
+
+
---
+++ gee2gpd/dfutils.py
@@ -0,0 +1,54 @@
+import warnings
+
+import geopandas as gpd
+import pandas as pd
+
+from timezonefinder import TimezoneFinder
+
+
+## UTC CONVERSTION UTILITY FUNCTIONS
+
+
+def insert_centroid_xy(df: gpd.GeoDataFrame):
+    df['x'] = df.geometry.centroid.x
+    df['y'] = df.geometry.centroid.y
+    return df
+
+
+def insert_time_zone(df: gpd.GeoDataFrame):
+    cols = df.columns.tolist()
+    if 'x' not in cols and 'y' not in cols:
+        raise ValueError("x,y coordinates of the centroid must be added")
+    tf = TimezoneFinder()
+    df["timezone"] = df.apply(lambda x: tf.timezone_at(lng=x["x"], lat=x["y"]), axis=1)
+    return df
+
+
+def convert_utc_time(df: gpd.GeoDataFrame, time_col: str) -> gpd.GeoDataFrame:
+    """ converts utc time to local time """
+    # date time conversion
+    cols = df.columns.tolist()
+    if 'timezone' not in cols or time_col not in cols:
+        raise ValueError
+
+    df["utc"] = df[time_col] / 1000.0
+    df["timestamp"] = pd.to_datetime(df["utc"], unit="s")
+    df["timestamp"] = df["timestamp"].dt.tz_localize("UTC")
+    df["timestamp"] = df.apply(
+        lambda row: row["timestamp"].tz_convert(row["timezone"]), axis=1
+    )
+    df["timestamp"] = pd.to_datetime(df["timestamp"], utc=True)
+    df["date"] = df["timestamp"].dt.strftime("%Y-%m-%d")
+    df["year"] = df["timestamp"].dt.year
+    df["month"] = df["timestamp"].dt.month
+    df["day"] = df["timestamp"].dt.day
+    df["julian_date"] = df["timestamp"].dt.dayofyear
+    return df
+
+
+def localize_utc(df, time_col: str = None):
+    time_col = time_col or 'system:time_start'
+    df = insert_centroid_xy(df)
+    df = insert_time_zone(df)
+    df = convert_utc_time(df, time_col)
+    return df[[i for i in df.columns.tolist() if i not in ['timestamp', 'utc', 'timezone', 'x', 'y']]]
